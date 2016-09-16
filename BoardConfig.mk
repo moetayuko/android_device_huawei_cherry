a07c88badff3007ecfd0c9428814be367fa5f1b6
dianlujitao <dianlujitao@gmail.com>
2016-09-16 13:27:39 +0800

    cherry: Clean up twrp tree, update from CM
    Change-Id: I4469ccc0a8bd9b79ce7626206f9f9b1f36489ed5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2016 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,16 +17,11 @@
 # Product-specific compile-time definitions.
 #
 
-LOCAL_PATH := device/huawei/cherry
-
-# Assert
-TARGET_OTA_ASSERT_DEVICE := c8817d,g620s,cherry
+DEVICE_PATH := device/huawei/cherry
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8916
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno306
-#add suffix variable to uniquely identify the board
-TARGET_BOARD_SUFFIX := _32
 
 # Bootloader
 TARGET_NO_BOOTLOADER := true
@@ -44,31 +39,28 @@ TARGET_CPU_SMP := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
 # Init
-TARGET_INIT_VENDOR_LIB := libinit_msm
+TARGET_INIT_VENDOR_LIB := libinit_cherry
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
-TARGET_LIBINIT_DEFINES_FILE := $(LOCAL_PATH)/init/init_cherry.c
+TARGET_RECOVERY_DEVICE_MODULES := libinit_cherry
 TARGET_UNIFIED_DEVICE := true
 
 # Kernel
-TARGET_CUSTOM_KERNEL_HEADERS := device/huawei/cherry/include
 BOARD_KERNEL_BASE        := 0x80000000
 BOARD_KERNEL_PAGESIZE    := 2048
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_RAMDISK_OFFSET     := 0x02000000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive
-BOARD_KERNEL_SEPARATED_DT := true
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02000000 --tags_offset 0x01e00000 --dt device/huawei/cherry/dt.img
-TARGET_PREBUILT_KERNEL := device/huawei/cherry/kernel
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02000000 --tags_offset 0x01e00000 --dt $(DEVICE_PATH)/dt.img
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/kernel
 
 # Partitions
-BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_PERSISTIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01400000 # (20M)
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01900000 # (25M)
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1288491008
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1070596096
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 1860648960
 BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
 BOARD_PERSISTIMAGE_PARTITION_SIZE := 33554432
@@ -76,35 +68,17 @@ BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
 
 # TWRP
 RECOVERY_VARIANT := twrp
-# TWRP_CN
-#TW_CUSTOM_THEME := $(LOCAL_PATH)/twrp/twres
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/twrp/twrp.fstab
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
+RECOVERY_GRAPHICS_FORCE_USE_LINELENGTH := true
 TW_THEME := portrait_hdpi
 RECOVERY_SDCARD_ON_DATA := true
-TW_TARGET_USES_QCOM_BSP := true
-TW_BOARD_CUSTOM_GRAPHICS := ../../../device/huawei/cherry/twrp/graphics.c
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-TW_INCLUDE_CRYPTO := true
-TW_FLASH_FROM_STORAGE := true
-TW_INTERNAL_STORAGE_PATH := "/data/media/0"
-TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
-TW_EXTERNAL_STORAGE_PATH := "/external_sd"
-TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
-TW_DEFAULT_EXTERNAL_STORAGE := true
-TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_NEW_ION_HEAP := true
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_EXTRA_LANGUAGES := true
+TW_INPUT_BLACKLIST := "accelerometer\x0alis3dh-accel"
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 BOARD_SUPPRESS_SECURE_ERASE := true
-TW_INPUT_BLACKLIST := "accelerometer"
-
-#Use dlmalloc instead of jemalloc for mallocs
-MALLOC_IMPL := dlmalloc
 
 # Vold
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
 BOARD_VOLD_MAX_PARTITIONS := 65
-
-# Include tzdata for recovery
-PRODUCT_COPY_FILES += \
-    bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
--- init/init_cherry.cpp
+++ init/init_cherry.cpp
@@ -27,152 +27,109 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#include <cstdlib>
+#include <fstream>
+#include <string>
 
 #include "vendor_init.h"
 #include "property_service.h"
 #include "log.h"
 #include "util.h"
 
-#include "init_msm.h"
+#define ISMATCH(a,b)    (!strncmp(a,b,PROP_VALUE_MAX))
 
-void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *board_type)
+void vendor_load_properties()
 {
     char platform[PROP_VALUE_MAX];
-    char model[110];
-    FILE* fp;
+    std::ifstream fin;
+    std::string buf;
     int rc;
-
-    UNUSED(msm_id);
-    UNUSED(msm_ver);
-    UNUSED(board_type);
 
     rc = property_get("ro.board.platform", platform);
     if (!rc || !ISMATCH(platform, ANDROID_TARGET))
         return;
 
-    fp = fopen("/proc/app_info", "rb");
-    while (fgets(model, 100, fp))
-        if (strstr(model, "huawei_fac_product_name") != NULL)
+    fin.open("/proc/app_info");
+    while (getline(fin, buf))
+        if (buf.find("huawei_fac_product_name") != std::string::npos)
             break;
+    fin.close();
 
     /* C8817D */
-    if (strstr(model, "C8817D") != NULL) {
+    if (buf.find("C8817D") != std::string::npos) {
         property_set("ro.product.model", "C8817D");
-        property_set("ro.product.name", "C8817D");
         property_set("ro.product.device", "C8817D");
         property_set("ro.build.product", "C8817D");
         property_set("persist.radio.multisim.config", "dsds");
-        property_set("persist.dsds.enabled", "true");
-        property_set("ro.telephony.default_network", "8");
-        property_set("telephony.lteOnCdmaDevice", "1");
-        property_set("ro.config.is_cdma_phone", "true");
-        property_set("ro.cdma.home.operator.numeric", "46003");
-        property_set("ro.config.cdma.globalMode", "true");
-        property_set("ro.config.dsds_mode", "cdma_gsm");
-        property_set("ro.com.android.dataroaming","true");
-        property_set("ro.ril.multi_rat_capable","true");
         property_set("ro.build.description", "C8817D-user 4.4.4 GRJ90 C92B381 release-keys");
         property_set("ro.build.fingerprint", "Huawei/C8817D/hwC8817D:4.4.4/HuaweiC8817D/C92B381:user/release-keys");
     }
     /* C8817E */
-    else if (strstr(model, "C8817E") != NULL) {
-        property_set("ro.product.model", "HUAWEI C8817E");
-        property_set("ro.product.name", "C8817E");
+    else if (buf.find("C8817E") != std::string::npos) {
+        property_set("ro.product.model", "C8817E");
         property_set("ro.product.device", "C8817E");
         property_set("ro.build.product", "C8817E");
-        property_set("ro.telephony.default_network", "8");
         property_set("ro.build.description", "C8817E-user 4.4.4 GRJ90 C92B380 release-keys");
         property_set("ro.build.fingerprint", "Huawei/C8817E/hwC8817E:4.4.4/HuaweiC8817E/C92B380:user/release-keys");
     }
     /* G621-TL00 */
-    else if (strstr(model, "G621-TL00") != NULL) {
+    else if (buf.find("G621-TL00") != std::string::npos) {
         property_set("ro.product.model", "G621-TL00");
-        property_set("ro.product.name", "G621-TL00");
         property_set("ro.product.device", "G621-TL00");
         property_set("ro.build.product", "G621-TL00");
         property_set("persist.radio.multisim.config", "dsds");
-        property_set("persist.dsds.enabled", "true");
-        property_set("ro.telephony.default_network", "17");
-        property_set("ro.config.dsds_mode", "umts_gsm");
-        property_set("ro.ril.multi_rat_capable","true");
         property_set("ro.build.description", "G621-TL00-user 4.4.4 GRJ90 C01B361SP01 release-keys");
         property_set("ro.build.fingerprint", "Honor/G621-TL00/hwG621-TL00:4.4.4/HonorG621-TL00/C01B361SP01:user/release-keys");
     }
     /* G620S-UL00 */
-    else if (strstr(model, "G620S-UL") != NULL) {
+    else if (buf.find("G620S-UL") != std::string::npos) {
         property_set("ro.product.model", "G620S-UL00");
-        property_set("ro.product.name", "G620S-UL00");
         property_set("ro.product.device", "G620S-UL00");
         property_set("ro.build.product", "G620S-UL00");
         property_set("persist.radio.multisim.config", "dsds");
-        property_set("persist.dsds.enabled", "true");
-        property_set("ro.telephony.default_network", "9");
-        property_set("ro.config.dsds_mode", "umts_gsm");
-        property_set("ro.ril.multi_rat_capable","true");
         property_set("ro.build.description", "G620S-UL00-user 4.4.4 GRJ90 C17B246 release-keys");
         property_set("ro.build.fingerprint", "Huawei/G620S-UL00/hwG620S-UL00:4.4.4/HuaweiG620S-UL00/C17B246:user/release-keys");
     }
     /* G620S-L01 */
-    else if (strstr(model, "G620S-L01") != NULL) {
+    else if (buf.find("G620S-L01") != std::string::npos) {
         property_set("ro.product.model", "G620S-L01");
-        property_set("ro.product.name", "G620S-L01");
         property_set("ro.product.device", "G620S-L01");
         property_set("ro.build.product", "G620S-L01");
-        property_set("ro.telephony.default_network", "9");
         property_set("ro.build.description", "G620S-L01-user 4.4.4 GRJ90 C00B380 release-keys");
         property_set("ro.build.fingerprint", "Huawei/G620S-L01/hwG620S-L01:4.4.4/HuaweiG620S-L01/C00B380:user/release-keys");
     }
-    /* Che1-CL20 */
-    else if (strstr(model, "Che1-CL20") != NULL) {
-        property_set("ro.product.model", "Che1-CL20");
-        property_set("ro.product.name", "4x");
-        property_set("ro.product.device", "Che1-CL20");
-        property_set("ro.build.product", "Che1-CL20");
-        property_set("persist.radio.multisim.config", "dsds");
-        property_set("persist.dsds.enabled", "true");
-        property_set("ro.telephony.default_network", "22");
-        property_set("telephony.lteOnCdmaDevice", "1");
-        property_set("ro.cdma.home.operator.numeric", "46003");
-        property_set("ro.config.cdma.globalMode", "true");
-        property_set("ro.config.dsds_mode", "cdma_gsm");
-        property_set("ro.ril.multi_rat_capable","true");
-        property_set("ro.build.description", "Che1-CL20-user 4.4.4 GRJ90 C00B275 release-keys");
-        property_set("ro.build.fingerprint", "Honor/Che1-CL20/Che1:4.4.4/Che1-CL20/C00B275:user/ota-rel-keys,release-keys");
+    /* G620S-L02 */
+    else if (buf.find("G620S-L02") != std::string::npos) {
+        property_set("ro.product.model", "G620S-L02");
+        property_set("ro.product.device", "G620S-L02");
+        property_set("ro.build.product", "G620S-L02");
+        property_set("ro.build.description", "G620S-L02-user 4.4.4 GRJ90 C00B380 release-keys");
+        property_set("ro.build.fingerprint", "Huawei/G620S-L02/hwG620S-L02:4.4.4/HuaweiG620S-L02/C00B380:user/release-keys");
     }
     /* Che1-CL10 */
-    else if (strstr(model, "Che1-CL10") != NULL) {
+    else if (buf.find("Che1-CL10") != std::string::npos) {
         property_set("ro.product.model", "Che1-CL10");
-        property_set("ro.product.name", "Che1-CL10");
         property_set("ro.product.device", "Che1-CL10");
         property_set("ro.build.product", "Che1-CL10");
         property_set("persist.radio.multisim.config", "dsds");
-        property_set("persist.dsds.enabled", "true");
-        property_set("ro.telephony.default_network", "8");
-        property_set("telephony.lteOnCdmaDevice", "1");
-        property_set("ro.cdma.home.operator.numeric", "46003");
-        property_set("ro.config.cdma.globalMode", "true");
-        property_set("ro.config.dsds_mode", "cdma_gsm");
-        property_set("ro.com.android.dataroaming","true");
-        property_set("ro.ril.multi_rat_capable","true");
         property_set("ro.build.description", "Che1-CL10-user 4.4.4 GRJ90 C92B275 release-keys");
         property_set("ro.build.fingerprint", "Honor/Che1-CL10/Che1:4.4.4/Che1-CL10/C92B275:user/ota-rel-keys,release-keys");
     }
+    /* Che1-CL20 */
+    else if (buf.find("Che1-CL20") != std::string::npos) {
+        property_set("ro.product.model", "Che1-CL20");
+        property_set("ro.product.device", "Che1-CL20");
+        property_set("ro.build.product", "Che1-CL20");
+        property_set("persist.radio.multisim.config", "dsds");
+        property_set("ro.build.description", "Che1-CL20-user 4.4.4 GRJ90 C00B275 release-keys");
+        property_set("ro.build.fingerprint", "Honor/Che1-CL20/Che1:4.4.4/Che1-CL20/C00B275:user/ota-rel-keys,release-keys");
+    }
     /* Che1-L04 */
-    else if (strstr(model, "Che1-L04") != NULL) {
+    else if (buf.find("Che1-L04") != std::string::npos) {
         property_set("ro.product.model", "Che1-L04");
-        property_set("ro.product.name", "Che1-L04");
         property_set("ro.product.device", "Che1-L04");
         property_set("ro.build.product", "Che1-L04");
         property_set("persist.radio.multisim.config", "dsds");
-        property_set("persist.dsds.enabled", "true");
-        property_set("ro.telephony.default_network", "9");
-        property_set("telephony.lteOnCdmaDevice", "1");
-        property_set("ro.config.dsds_mode", "umts_gsm");
-        property_set("ro.com.android.dataroaming","true");
-        property_set("ro.ril.multi_rat_capable","true");
         property_set("ro.build.description", "Che1-L04-user 4.4.4 GRJ90 C900B130 release-keys");
         property_set("ro.build.fingerprint", "Honor/Che1-L04/Che1:4.4.4/Che1-L04/C900B130:user/ota-rel-keys,release-keys");
     }
