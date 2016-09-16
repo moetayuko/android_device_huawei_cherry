4bb4353f472663fc119482f03f5ed0fcce92cd38
dianlujitao <dianlujitao@gmail.com>
2016-09-16 16:32:04 +0800

    cherry: Fix init extension build on N
     * Also remove build description/fingerprint override, the device
       failed to boot with them.
    
    Change-Id: I094bdf2f66afe7a3668b6d6d76f9e6cecf713e95

--- init/init_cherry.cpp
+++ init/init_cherry.cpp
@@ -28,11 +28,12 @@
  */
 
 #include <cstdlib>
+#include <unistd.h>
 #include <fstream>
 #include <string>
 
+#include <cutils/properties.h>
 #include "vendor_init.h"
-#include "property_service.h"
 #include "log.h"
 #include "util.h"
 
@@ -45,11 +46,14 @@ void vendor_load_properties()
     std::string buf;
     int rc;
 
-    rc = property_get("ro.board.platform", platform);
+    rc = property_get("ro.board.platform", platform, NULL);
     if (!rc || !ISMATCH(platform, ANDROID_TARGET))
         return;
 
-    fin.open("/proc/app_info");
+    fin.open("/proc/app_info", std::ios::in);
+    if (!fin)
+        return;
+
     while (getline(fin, buf))
         if (buf.find("huawei_fac_product_name") != std::string::npos)
             break;
@@ -61,16 +65,12 @@ void vendor_load_properties()
         property_set("ro.product.device", "C8817D");
         property_set("ro.build.product", "C8817D");
         property_set("persist.radio.multisim.config", "dsds");
-        property_set("ro.build.description", "C8817D-user 4.4.4 GRJ90 C92B381 release-keys");
-        property_set("ro.build.fingerprint", "Huawei/C8817D/hwC8817D:4.4.4/HuaweiC8817D/C92B381:user/release-keys");
     }
     /* C8817E */
     else if (buf.find("C8817E") != std::string::npos) {
         property_set("ro.product.model", "C8817E");
         property_set("ro.product.device", "C8817E");
         property_set("ro.build.product", "C8817E");
-        property_set("ro.build.description", "C8817E-user 4.4.4 GRJ90 C92B380 release-keys");
-        property_set("ro.build.fingerprint", "Huawei/C8817E/hwC8817E:4.4.4/HuaweiC8817E/C92B380:user/release-keys");
     }
     /* G621-TL00 */
     else if (buf.find("G621-TL00") != std::string::npos) {
@@ -78,8 +78,6 @@ void vendor_load_properties()
         property_set("ro.product.device", "G621-TL00");
         property_set("ro.build.product", "G621-TL00");
         property_set("persist.radio.multisim.config", "dsds");
-        property_set("ro.build.description", "G621-TL00-user 4.4.4 GRJ90 C01B361SP01 release-keys");
-        property_set("ro.build.fingerprint", "Honor/G621-TL00/hwG621-TL00:4.4.4/HonorG621-TL00/C01B361SP01:user/release-keys");
     }
     /* G620S-UL00 */
     else if (buf.find("G620S-UL") != std::string::npos) {
@@ -87,24 +85,18 @@ void vendor_load_properties()
         property_set("ro.product.device", "G620S-UL00");
         property_set("ro.build.product", "G620S-UL00");
         property_set("persist.radio.multisim.config", "dsds");
-        property_set("ro.build.description", "G620S-UL00-user 4.4.4 GRJ90 C17B246 release-keys");
-        property_set("ro.build.fingerprint", "Huawei/G620S-UL00/hwG620S-UL00:4.4.4/HuaweiG620S-UL00/C17B246:user/release-keys");
     }
     /* G620S-L01 */
     else if (buf.find("G620S-L01") != std::string::npos) {
         property_set("ro.product.model", "G620S-L01");
         property_set("ro.product.device", "G620S-L01");
         property_set("ro.build.product", "G620S-L01");
-        property_set("ro.build.description", "G620S-L01-user 4.4.4 GRJ90 C00B380 release-keys");
-        property_set("ro.build.fingerprint", "Huawei/G620S-L01/hwG620S-L01:4.4.4/HuaweiG620S-L01/C00B380:user/release-keys");
     }
     /* G620S-L02 */
     else if (buf.find("G620S-L02") != std::string::npos) {
         property_set("ro.product.model", "G620S-L02");
         property_set("ro.product.device", "G620S-L02");
         property_set("ro.build.product", "G620S-L02");
-        property_set("ro.build.description", "G620S-L02-user 4.4.4 GRJ90 C00B380 release-keys");
-        property_set("ro.build.fingerprint", "Huawei/G620S-L02/hwG620S-L02:4.4.4/HuaweiG620S-L02/C00B380:user/release-keys");
     }
     /* Che1-CL10 */
     else if (buf.find("Che1-CL10") != std::string::npos) {
@@ -112,8 +104,6 @@ void vendor_load_properties()
         property_set("ro.product.device", "Che1-CL10");
         property_set("ro.build.product", "Che1-CL10");
         property_set("persist.radio.multisim.config", "dsds");
-        property_set("ro.build.description", "Che1-CL10-user 4.4.4 GRJ90 C92B275 release-keys");
-        property_set("ro.build.fingerprint", "Honor/Che1-CL10/Che1:4.4.4/Che1-CL10/C92B275:user/ota-rel-keys,release-keys");
     }
     /* Che1-CL20 */
     else if (buf.find("Che1-CL20") != std::string::npos) {
@@ -121,8 +111,6 @@ void vendor_load_properties()
         property_set("ro.product.device", "Che1-CL20");
         property_set("ro.build.product", "Che1-CL20");
         property_set("persist.radio.multisim.config", "dsds");
-        property_set("ro.build.description", "Che1-CL20-user 4.4.4 GRJ90 C00B275 release-keys");
-        property_set("ro.build.fingerprint", "Honor/Che1-CL20/Che1:4.4.4/Che1-CL20/C00B275:user/ota-rel-keys,release-keys");
     }
     /* Che1-L04 */
     else if (buf.find("Che1-L04") != std::string::npos) {
@@ -130,7 +118,5 @@ void vendor_load_properties()
         property_set("ro.product.device", "Che1-L04");
         property_set("ro.build.product", "Che1-L04");
         property_set("persist.radio.multisim.config", "dsds");
-        property_set("ro.build.description", "Che1-L04-user 4.4.4 GRJ90 C900B130 release-keys");
-        property_set("ro.build.fingerprint", "Honor/Che1-L04/Che1:4.4.4/Che1-L04/C900B130:user/ota-rel-keys,release-keys");
     }
 }
